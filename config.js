b9a7c6246d5ff1f03c7a5780243ab90018330244
vivianhyde1987 <vivianhyde1987@users.noreply.github.com>
2026-05-30 13:21:02 +0800

    Configure Supabase guestbook

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ROSE_BLOG_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://sktukovuepsckagptmap.supabase.co",
+  supabaseAnonKey: "sb_publishable_KpLfIyQObojeYSq79-9WGw_ZK7R4BF5"
 };
